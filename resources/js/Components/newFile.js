af55033b38377370e077f798d664f55e3a385933
suzusan555 <ryuusei.8hatake@gmail.com>
2022-09-30 16:12:30 +0900

    inertia-vue.js

---
+++ resources/js/Components/newFile.js
@@ -0,0 +1,18 @@
+import { Link } from '@inertiajs/inertia-vue3';
+
+export default await (async () => {
+const __VLS_setup = async () => {
+const __VLS_Component = (await import('vue')).defineComponent({
+setup() {
+return {};
+}
+});
+function __VLS_template() {
+// @ts-ignore
+[Link];
+return {};
+}
+return {} as typeof __VLS_Component & (new () => { $slots: ReturnType<typeof __VLS_template>; });
+};
+return await __VLS_setup();
+})();
---
+++ resources/js/Pages/ComponentTest.vue
@@ -0,0 +1,14 @@
+<script setup>
+import GuestLayouts from '@/Layouts/GuestLayout.vue'
+import LabelInputs from '@/Components/InputLabel.vue'
+import TextInput from '@/Components/TextInput.vue'
+</script>
+
+<template>
+  <GuestLayouts>
+    <LabelInputs value="件名">
+      いいい
+    </LabelInputs>
+    <TextInput modelValue="初期値"></TextInput>
+  </GuestLayouts>
+</template>
